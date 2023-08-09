b8d2ae9dcf1b437a69627689339322c89683bfa1
VentOSi <98553334+VentOSi@users.noreply.github.com>
2023-08-09 15:30:26 +0300

    First step. Create gendiff

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+	.name('gendiff')
+	.description('Compares two configuration files and shows a difference.')
+	.version('11.0.0', '-v, --vers', 'output the version number')
+	.helpOption('-h, --help', 'output usage information')
+	.parse()
